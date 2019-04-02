92c711d9106631edf49806f6ee7e1e11f570203f
dthill <dthill@gmx.net>
2019-04-02 12:39:57 +0200

    Updates

---
+++ precache-manifest.1627b4696d0bc98c8df688abdf9927f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cf17f7f71b36deef5e6",
+    "url": "/react-endless-wiki-viewer/static/js/main.eb2432c4.chunk.js"
+  },
+  {
+    "revision": "ddced609a9bfc46b77fd",
+    "url": "/react-endless-wiki-viewer/static/js/runtime~main.f6a78838.js"
+  },
+  {
+    "revision": "e8a04943b425ab1132f3",
+    "url": "/react-endless-wiki-viewer/static/js/2.825f385e.chunk.js"
+  },
+  {
+    "revision": "3019d7e713391b71b702cf4fa0a94ec0",
+    "url": "/react-endless-wiki-viewer/index.html"
+  }
+];
